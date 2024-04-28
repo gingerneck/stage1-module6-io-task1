ce02df7fd0889e7feae0903d2d82ae2af541c513
Nikita Pyshchev <npyshchev@igatec.com>
2024-04-28 17:55:07 +0300

    wip

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,67 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        var res = new Profile();
+        try {
+            try (var reader = new java.io.FileReader(file);
+                 var bufferReader = new BufferedReader(reader)) {
+                String line;
+                while ((line = bufferReader.readLine()) != null) {
+                    setData(line, res);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return res;
+    }
+
+    private void setData(String data, Profile profile) {
+        var key = data.substring(0, data.indexOf(":")).toLowerCase();
+        var value = data.substring(data.indexOf(":") + 1).trim();
+        switch (key) {
+            case ("name"): {
+                profile.setName(value);
+                break;
+            }
+            case ("age"): {
+                profile.setAge(getInteger(value));
+            }
+            case ("email"): {
+                profile.setEmail(value);
+            }
+            case ("phone"): {
+                profile.setPhone((long) getInteger(value));
+            }
+        }
+    }
+
+    private int getInteger(String value) {
+        char[] chars = value.toCharArray();
+        boolean isNegative = (chars[0] == '-');
+        if (isNegative) {
+            chars[0] = '0';
+        }
+
+        int multiplier = 1;
+        int total = 0;
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            total = total + ((chars[i] - '0') * multiplier);
+            multiplier = multiplier * 10;
+        }
+
+        if (isNegative) {
+            total = total * -1;
+        }
+        return total;
     }
 }
